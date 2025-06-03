96604b923d55db7bb1b8d7b135e50ab1f4ac5cab
Yuri <yuri.pillmen@gmail.com>
2025-06-02 21:10:09 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MiService } from './demo/service';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
+  providers: [
+    MiService
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'viaandina-app';
+
+  schedules: any = [];
+
+  constructor(private miService: MiService) {}
+
+  ngOnInit(): void {
+    this.getRoutes();
+  }
+
+  getRoutes():void {
+    this.miService.getData()
+      .subscribe(data => {
+        console.log(data);
+        this.schedules = data;
+      })
+  }
+  
 }
---
+++ src/app/demo/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MiService {
+
+  private apiUrl: string = 'http://localhost:8090/api/';
+  private service: string = 'scheduler'
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    let url = `${this.apiUrl}${this.service}/schedules`
+    return this.http.get<any>(url);
+  }
+}
